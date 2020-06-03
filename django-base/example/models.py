6b776e2685fd25c838e7fe6de1eea8f112031ded
Shawn Cicoria <github@cicoria.com>
2020-06-03 19:10:44 -0400

    working

--- django-base/example/models.py
+++ django-base/example/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class BakedGood(models.Model):
+    name = models.CharField(max_length=64)
+    desc = models.CharField(max_length=256)
+    good_type = models.TextChoices('type', 'BAGEL BREAD COOKIE CAKE PRETZEL')
+    sTypes = models.CharField(blank=True, choices=good_type.choices, max_length=20)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    recipe = models.TextField()
+
+
+class OtherBakedGood(models.Model):
+    name = models.CharField(max_length=64)
+    desc = models.CharField(max_length=256)
+    goodType = models.TextChoices('goodType', 'BAGEL BREAD COOKIE CAKE PRETZEL')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    recipe = models.TextField()
