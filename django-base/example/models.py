de45172d9d15b9b025fbf33d3fce5342c218a375
Shawn Cicoria <github@cicoria.com>
2020-06-03 18:08:54 -0400

    updates week 3

--- django-base/example/models.py
+++ django-base/example/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class BakedGood(models.Model):
+    name = models.CharField(max_length=64)
+    desc = models.CharField(max_length=256)
+    good_type = models.TextChoices('type', 'BAGEL BREAD COOKIE CAKE PRETZEL')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    recipe = models.TextField()
